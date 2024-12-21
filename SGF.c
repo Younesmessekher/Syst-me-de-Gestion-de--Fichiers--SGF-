d462999a4ccf722cf02fb366357fa8a6574661c5
Younesmessekher <younesmessekher791@gmail.com>
2024-12-21 13:58:04 +0100

    Update SGF.c

--- SGF.c
+++ SGF.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#define FB 3 // FB = facture de blockage 
+#define nbrBlock 10  // nbrBlock = nombre des blockes dans MS 
+ 
+ // déclaration des structures 
 
 
+
+ //Mémoire Secondaire (MS)
+ typedef struct {
+  
+
+ }MS; 
 
 
 int main() {
